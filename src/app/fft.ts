f816b42489a73cb43571334d1a0943ba1b6fdb87
Gareth Stephenson <gareth.stephenson@gmail.com>
2017-12-14 20:51:09 +0200

    Have added the right colours and metrics for LEVEL (db) in positive readings, graph scales accordingly on resize

--- src/app/fft.ts
+++ src/app/fft.ts
@@ -40,9 +40,13 @@ export class FFT {
 
         // ... initialise static chart area
 
-        const margin = { top: 20, right: 20, bottom: 50, left: 40 };
-        const width = 960 - margin.left - margin.right;
-        const height = 480 - margin.top - margin.bottom;
+        const margin = { top: 20, right: 20, bottom: 150, left: 40 };
+        // const width = 960 - margin.left - margin.right;
+        const width = document.documentElement.getBoundingClientRect().width - margin.left - margin.right;
+        // const height = 480 - margin.top - margin.bottom;
+        const height = document.documentElement.getBoundingClientRect().height - margin.top - margin.bottom;
+
+console.log('width: ' + width + ', height: ' + height);
 
         const x = d3.scaleLinear()
             .range([0, width])
@@ -50,14 +54,16 @@ export class FFT {
 
         const y = d3.scaleLinear()
             .range([height, 0])
-            .domain([-80, 0]);
+            .domain([0, 100]);
+            // .domain([-80, 0]);
 
         const xAxis = d3.axisBottom(x)
             .ticks(10)
             .tickFormat(d3.format('d'));
 
         const yAxis = d3.axisLeft(y)
-            .ticks(10);
+            .ticks(6)
+            .tickFormat(d3.format('d'));
 
         const svg = d3.select('#spectrum').append('svg')
             .attr('id', 'fft')
@@ -82,7 +88,7 @@ export class FFT {
         const rect = body.append('svg:rect')
             .attr('width', width)
             .attr('height', height)
-            .attr('fill', 'white');
+            .attr('fill-opacity', '0');
 
         // ... setup FFT line
 
@@ -90,11 +96,11 @@ export class FFT {
             // .interpolate("cardinal")
             .x(function (d) { return x(d.frequency); })
             .y0(height)
-            .y1(function (d) { return y(d.dbv); })
+            .y1(function (d) { return y(100 + d.dbv); })
             .curve(d3.curveCardinal);
 
         body.append('path')
-            .data([FFT])
+            .data([FFT.FFTs])
             .attr('id', 'fftx')
             .attr('class', 'fft')
             .attr('d', FFT.fft);
@@ -119,6 +125,7 @@ export class FFT {
 
         svg.append('g')
             .attr('class', 'grid')
+            .attr('stroke-opacity', '0')
             .attr('transform', 'translate(0,' + height + ')')
             .call(this.make_x_axis(x)
                 .tickSize(-height, 0, 0)
@@ -126,6 +133,7 @@ export class FFT {
 
         svg.append('g')
             .attr('class', 'grid')
+            .attr('stroke-opacity', '0')
             .call(this.make_y_axis(y, 10)
                 .tickSize(-width, 0, 0)
                 .tickFormat(''));
@@ -133,37 +141,42 @@ export class FFT {
         // ... draw X-axis label
 
         svg.append('g')
+            // .attr('id', 'garethx')
             .attr('class', 'x axis')
             .attr('transform', 'translate(0,' + height + ')')
             .call(xAxis)
             .append('text')
-            .attr('x', width)
+            .attr('x', width / 2)
             .attr('dy', '3.5em')
             .style('text-anchor', 'end')
-            .text('Frequency (kHz)');
+            .text('FREQUENCY (kHz)');
 
         // ... draw Y-axis label
 
         svg.append('g')
+            // .attr('id', 'garethy')
             .attr('class', 'y axis')
             .call(yAxis)
+            .append('g')
+            // .style('text-anchor', 'end')
             .append('text')
             .attr('y', 6)
-            .attr('dx', '1em')
-            .attr('dy', '-1.75em')
-            .style('text-anchor', 'end')
-            .text('dB');
+            .attr('x', -(height / 2))
+            .attr('dy', '-2.75em')
+            .style('text-anchor', 'start')
+            .attr('transform', 'rotate(270,12,19)')
+            .text('LEVEL (dB)');
 
         // ... draw outline
 
-        svg.append('rect')
-            .attr('x', 0)
-            .attr('y', 0)
-            .attr('height', height)
-            .attr('width', width)
-            .attr('fill', 'none')
-            .attr('stroke', '#808080')
-            .attr('stroke-width', 1);
+        // svg.append('rect')
+        //     .attr('x', 0)
+        //     .attr('y', 0)
+        //     .attr('height', height)
+        //     .attr('width', width)
+        //     .attr('fill', 'none')
+        //     .attr('stroke', '#808080')
+        //     .attr('stroke-width', 1);
     }
 
     fft_draw(data) {
--- src/app/spectrum/spectrum.component.ts
+++ src/app/spectrum/spectrum.component.ts
@@ -45,9 +45,11 @@ export class SpectrumComponent implements AfterViewInit, OnDestroy {
             case 'fft':
                 SpectrumComponent.fft.fft_draw(data.fft);
                 break;
+/*
             case 'metrics':
                 SpectrumComponent.fft.metrics_draw(data.samples);
                 break;
+*/
         }
     }
 
@@ -62,76 +64,8 @@ export class SpectrumComponent implements AfterViewInit, OnDestroy {
     render() {
         this.fft.fft_setup();
 
-        /*
-        d3.select('#spectrum > svg').remove();
-        this.svg = d3.select('#spectrum').append('svg');
-        const margin = { top: 20, right: 40, bottom: 30, left: 40 },
-            width = parseInt(this.svg.style('width'), 10) - margin.left - margin.right,
-            height = parseInt(this.svg.style('height'), 10) - margin.top - margin.bottom,
-            g = this.svg.append('g').attr('transfor', 'translate(' + margin.left + ',' + margin.top + ')');
-
-        const parseTime = d3.timeParse('%d-%b-%y');
-
-        const x = d3.scaleTime()
-            .rangeRound([0, width]);
-
-        const y = d3.scaleLinear()
-            .rangeRound([height, 0]);
-
-            const area = d3.area()
-            .x(function (d) {
-                return x(d.date);
-            })
-            .y1(function (d) {
-                return y(d.close);
-            });
-
-        d3.tsv("assets/areas.tsv", function (d) {
-            d.date = parseTime(d.date);
-            d.close = +d.close;
-            return d;
-        }, (error, data) => {
-            if (error) throw error;
-
-            x.domain(d3.extent(data, function (d) {
-                return d.date;
-            }));
-            y.domain([0, d3.max(data, function (d) {
-                return d.close;
-            })]);
-            area.y0(y(0));
-
-            g.append("path")
-                .datum(data)
-                .attr("fill", "steelblue")
-                .attr("d", area);
-
-            g.append("g")
-                .attr("transform", "translate(0," + height + ")")
-                .attr("class", this.graphConfig.xAxisName)
-                .style("stroke", this.graphConfig.stroke)
-                .style("border", this.graphConfig.borderWidth)
-                .style("border-style", this.graphConfig.borderStyle)
-                .style("border-color", this.graphConfig.borderColor)
-                .attr("transform", "translate(0," + height + ")")
-                .call(d3.axisBottom(x));
-
-            g.append("g")
-                .attr("y", 6)
-                .attr("dy", "0.71em")
-                .attr("text-anchor", "end")
-                .text("Price ($)")
-                .attr("class", this.graphConfig.yAxisName)
-                .style("stroke", this.graphConfig.stroke)
-                .style("border", this.graphConfig.borderWidth)
-                .style("border-style", this.graphConfig.borderStyle)
-                .style("border-color", this.graphConfig.borderColor)
-                .call(d3.axisLeft(y))
-                .append("text");
-        });
-        */
-
         d3.select(window).on('resize', () => {
+            d3.select('#spectrum > svg').remove();
             this.render();
         });
     }
